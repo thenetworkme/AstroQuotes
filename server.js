8ab7d5cf498adbe5fa2a75cdf637ab865f7de90b
BugNetwork <rainelro21@gmail.com>
2024-05-30 11:23:06 -0400

    cors activated for origin

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 const app = express();
 const port = 3000;
