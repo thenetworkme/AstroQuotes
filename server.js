55d95bedeb200f36b6e85207de575db74ccdb441
BugNetwork <rainelro21@gmail.com>
2024-05-29 21:18:26 -0400

    Data transfered to a file

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.get("/api/posts", (req, res) => {
-  const posts = [
-    { id: 1, title: "Post 1", body: "Contenido del post 1" },
-    { id: 2, title: "Post 2", body: "Contenido del post 2" },
-    { id: 3, title: "Post 3", body: "Contenido del post 3" },
-  ];
-
-  res.json(posts);
+app.get("/api/quotes", (req, res) => {
+  const file = path.join(__dirname, "data.json");
+  fs.readFile(file, "utf-8", (err, data) => {
+    try {
+      const datos = JSON.parse(data);
+      res.json(datos);
+    } catch (error) {
+      console.error("Ha ocurrido un error con la lectura de datos");
+      res.status(500);
+    }
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en puerto ${port}`);
 });
